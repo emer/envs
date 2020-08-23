d5d8975912a65724f2bf311574abc9db29fc3316
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2020-08-23 13:45:52 -0700

    add a dummy top-level doc file

---
+++ doc.go
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, The Emergent Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// package envs is a dummy placeholder so you don't get warning errors about no .go files.
+// see the subdirectories for all the relevant code, which is typically main packages
+// that are intended to be copied to projects and modified as needed.
+package envs
